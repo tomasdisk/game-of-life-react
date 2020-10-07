06fdeaadb1a39d9434502d9dd672346d6c2c4cf5
tomasdisk <tomasdisk@hotmail.com>
2020-10-07 02:01:43 -0300

    Game of Life: Initial build

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Game of Life
 
 Game of life implemented in P5.js wrapped on React.js app
+
+Live on https://tomasdisk.github.io/game-of-life-react
---
+++ build/precache-manifest.173a9b469892bf376d907918fdd39a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea4812d355577e8439fdef972a5a1de",
+    "url": "/game-of-life-react/index.html"
+  },
+  {
+    "revision": "470b160279d4e6e68eba",
+    "url": "/game-of-life-react/static/css/main.983323aa.chunk.css"
+  },
+  {
+    "revision": "9e986a7efb41690760ac",
+    "url": "/game-of-life-react/static/js/2.abe55085.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life-react/static/js/2.abe55085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470b160279d4e6e68eba",
+    "url": "/game-of-life-react/static/js/main.c40fbadd.chunk.js"
+  },
+  {
+    "revision": "dbf4b43ee54679668947",
+    "url": "/game-of-life-react/static/js/runtime-main.4a1f16b3.js"
+  }
+]);
